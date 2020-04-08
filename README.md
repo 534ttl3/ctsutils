dd17a52a8b78d1c1ed4f5c33a7e21675961c0884
534ttl3 <534ttl3@gmail.com>
2020-04-08 21:33:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Utils for Python
+
+Often I need to plot things with matplotlib. This library defines 
+utility functions for example for this usecase.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="534ttl3", # Replace with your own username
+    version="0.0.1",
+    author="C.T. Schnur",
+    author_email="534ttl3@gmail.com",
+    description="Utility functions for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/534ttl3/csutils/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
